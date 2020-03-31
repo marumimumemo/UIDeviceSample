862a3070b55e0786120566365166cdecfd2fd7e9
satoshi.marumoto <satoshi.marumoto@st-ventures.jp>
2020-03-31 11:47:49 +0900

    UIDeviceSample

--- UIDeviceSample/ViewController.swift
+++ UIDeviceSample/ViewController.swift
@@ -12,9 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // デバイスのOS名称
+        print(UIDevice.current.systemName)
+        // OSバージョン
+        print(UIDevice.current.systemVersion)
+        // 端末名称
+        print(UIDevice.current.name)
+        // 端末のモデル
+        print(UIDevice.current.model)
+        // iPhone/iPadの判別
+        print(UIDevice.current.userInterfaceIdiom)
+        // 表示されている画面の向きを取得
+        print(UIDevice.current.orientation)
+        // マルチタスクをサポートしているか
+        print(UIDevice.current.isMultitaskingSupported)
+        // アプリ内でバッテリーの状態を取得
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        // バッテリーの有無の状態を取得
+        print(UIDevice.current.batteryLevel)
+        // バッテリー状態を取得
+        print(UIDevice.current.batteryState)
+        // 接近センサーの有効無効を設定
+        UIDevice.current.isProximityMonitoringEnabled = true
+        // 接近センサーの状態を取得
+        print(UIDevice.current.proximityState)
     }
-
-
 }
 
